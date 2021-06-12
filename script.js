5b778fb9b63323e1ee992151b639f1a8a1165896
AlexHolad <alexej.holad@gmail.com>
2021-06-12 16:04:34 +0200

    practice Array

---
+++ script.js
@@ -0,0 +1,43 @@
+// const(fruits = ["Kiwi", "Apple", "Pineapple"];
+// console.log(fruits[0]); // Will print "Kiwi"
+// console.log(fruits[1]); // Will print "Apple"
+// console.log(fruits[2]); // Will print "Pineapple'
+// fruits[0] = "Banana"; // Will change the first element to Banana
+// console.log(fruits[0]); // Will print "Banana"
+
+// const animals = ["Tiger", "Lion", "Monkey"]
+// console.log(animals[0])
+// console.log(animals[1])
+// console.log(animals[2])  
+// console.log(animals[0])
+
+// console.log(animals.length);
+
+// animals.push("Elephant")
+
+// console.log(animals)
+
+// animals.pop()
+
+// console.log(animals)
+
+// const fruits1 = ["Apple", "Strawberry"];
+// const fruits2 = ["Banana", "Pineapple"];
+
+// const mySmoothie = fruits1.concat(fruits2);
+// console.log(mySmoothie);
+
+// const numbers = [1, 20, 45, 2, 3, 5, 8];
+// console.log(numbers.sort())
+
+// const numbers = [1, 20, 45, 2, 3, 5, 8];
+// console.log(numbers.sort(function(a, b){
+// 	return a - b;
+// }));
+
+const fruits = ["Kiwi", "Apple", "Pineapple"];
+console.log(fruits.includes("Kiwi")); // true
+console.log(fruits.includes("Banana")); // false
+
+const animals = ["Lion", "Monkey", "Tiger"];
+console.log(animals.includes("Lion"))
